0b39b785eed6681ad33340fc8246d2a467d79746
Gaute Rønningen <19590971+GauteR@users.noreply.github.com>
2019-04-05 14:11:20 +0200

    Moved folders, added autoprefixer and minifier

--- GruntFile.js
+++ GruntFile.js
@@ -2,22 +2,43 @@ const sass = require('node-sass');
 
 module.exports = function(grunt) {
 
-  grunt.initConfig({
-		sass: {
-			options: {
-				implementation: sass,
-				sourceMap: false
-			},
-			dist: {
-				files: {
-					'css/example.css': 'scss/example.scss'
+	grunt.initConfig({
+			sass: {
+				options: {
+					implementation: sass,
+					sourceMap: false
+				},
+				dist: {
+					files: {
+						'dist/css/example.css': 'src/scss/example.scss'
+					}
 				}
+			},
+			postcss: {
+				options: {
+					map: false, // inline sourcemaps
+			
+					processors: [
+					require('pixrem')(), // add fallbacks for rem units
+					require('autoprefixer')({browsers: 'last 2 versions'}), // add vendor prefixes
+					require('cssnano')() // minify the result
+					]
+				},
+				dist: {
+					files: {
+						'dist/css/example.min.css': 'dist/css/example.css'
+					}
+				}
+			},
+			clean: {
+				dist: ['dist/**/*.css'] // Clean all css
 			}
-		}
-  });
+	});
 
-  grunt.loadNpmTasks('grunt-sass');
+	grunt.loadNpmTasks('grunt-contrib-clean');
+	grunt.loadNpmTasks('grunt-sass');
+	grunt.loadNpmTasks('grunt-postcss');
 
-  grunt.registerTask('default', ['sass']);
+	grunt.registerTask('default', ['clean', 'sass', 'postcss']);
 
 };
